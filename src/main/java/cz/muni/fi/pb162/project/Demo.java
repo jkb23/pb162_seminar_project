da9416dcd58091322526bd32b36a8a62bab56937
Matus Jakab <xjakab1@fi.muni.cz>
2022-02-22 09:30:55 +0100

    01 looks done, tests passed

--- src/main/java/cz/muni/fi/pb162/project/Demo.java
+++ src/main/java/cz/muni/fi/pb162/project/Demo.java
@@ -1,10 +1,11 @@
 package cz.muni.fi.pb162.project;
 
+import cz.muni.fi.pb162.project.geometry.Vertex2D;
 
 /**
  * Class for running main method.
  *
- * @author TODO: put your name here
+ * @author Matus Jakab
  */
 public class Demo {
 
@@ -14,8 +15,17 @@ public class Demo {
      * @param args command line arguments, will be ignored
      */
     public static void main(String[] args) {
+        Vertex2D vertex = new Vertex2D();
+        vertex.setX(2);
+        vertex.setY(3);
 
-        System.out.println("Hello world!");
+        Vertex2D vertex2 = new Vertex2D();
+        vertex2.setX(1);
+        vertex2.setY(1);
 
+        vertex.move(vertex2);
+
+        System.out.println(vertex.getInfo());
+        System.out.println(vertex2.getInfo());
     }
 }
